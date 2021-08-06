6d425b8d7fac2a838ef6de12113d6dc4a7bf8272
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-08-06 15:26:09 -0400

    reconfigure folder structure

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,14 +1,20 @@
 const sayHello = require('../utils/employee');
 
+const mockConsole = jest.spyOn(console, 'log');
+const mockConImp = mockConsole.mockImplementation(() => { });
+
 describe('Employee', () => {
     describe('sayHello', () => {
         it('should return the statement: say hello', () => {
+            mockConsole;
+            mockConImp;
+            mock.mockRestore();
             const string = 'Hello World!';
 
             const result = sayHello(string);
 
-            expect(result).toEqual(string);
 
+            expect(result).toEqual(string);
 
         })
     })
