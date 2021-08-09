be7ecce82e885f7bc954c1e5b72301290cfade0d
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-08-09 12:17:32 -0400

    loop through engineers and render on html

--- src/page-template.js
+++ src/page-template.js
@@ -39,7 +39,24 @@ function managerCard(manager) {
 }
 
 function engineerCard(engineer) {
-    console.log(engineer);
+    let engineerCardStr = '';
+
+    // || Loop through array of engineers and concatenate strings together each time
+    for (let i = 0; i < engineer.length; i++) {
+        engineerCardStr = engineerCardStr + `
+    <div class="card">
+        <h2>${engineer[i].name}</h2>
+        <h3>${engineer[i].getRole()}</h3>
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer[i].id}</li >
+            <li class="list-group-item">Email: ${engineer[i].email}</li>
+            <li class="list-group-item">Office Number: ${engineer[i].github}</li>
+        </ul >
+    </div >
+        `
+    }
+    return engineerCardStr;
+
 };
 
 function internCard(internsArr) {
@@ -48,27 +65,27 @@ function internCard(internsArr) {
 
 function generateHtml(manager, engineer, intern) {
     console.log(manager);
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-        <title>Team Profile</title>
+    return `< !DOCTYPE html >
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+                                <title>Team Profile</title>
     </head>
-    <body>
-        <header>
-        <h1>My Team</h1>
-        </header>
+                            <body>
+                                <header>
+                                    <h1>My Team</h1>
+                                </header>
 
-        ${managerCard(manager)}
-        ${engineerCard(engineer)}
+                                ${managerCard(manager)}
+                                ${engineerCard(engineer)}
 
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-    </body>
+                                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+                            </body>
     </html>`;
 }
 
