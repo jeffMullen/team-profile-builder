65af9335c3b886d0273ecce954520e97df5d5564
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-08-09 12:35:18 -0400

    cards for each employee show up on html

--- src/page-template.js
+++ src/page-template.js
@@ -26,7 +26,7 @@ function sortEmployees(array) {
 function managerCard(manager) {
     console.log(manager);
     return `
-    <div class="card">
+    <div class="card p-3 m-3">
         <h2>${manager[0].name}</h2>
         <h3>${manager[0].getRole()}</h3>
         <ul class="list-group">
@@ -44,7 +44,7 @@ function engineerCard(engineer) {
     // || Loop through array of engineers and concatenate strings together each time
     for (let i = 0; i < engineer.length; i++) {
         engineerCardStr = engineerCardStr + `
-    <div class="card">
+    <div class="card p-3 m-3">
         <h2>${engineer[i].name}</h2>
         <h3>${engineer[i].getRole()}</h3>
         <ul class="list-group">
@@ -65,7 +65,7 @@ function internCard(intern) {
     // || Loop through array of engineers and concatenate strings together each time
     for (let i = 0; i < intern.length; i++) {
         internCardStr = internCardStr + `
-    <div class="card">
+    <div class="card p-3 m-3">
         <h2>${intern[i].name}</h2>
         <h3>${intern[i].getRole()}</h3>
         <ul class="list-group">
@@ -83,26 +83,31 @@ function generateHtml(manager, engineer, intern) {
     console.log(manager);
     return `<!DOCTYPE html>
         <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-                                <title>Team Profile</title>
-    </head>
-                            <body>
-                                <header>
-                                    <h1>My Team</h1>
-                                </header>
+        <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <title>Team Profile</title>
+        </head>
+        <body>
+            <header>
+                <h1>My Team</h1>
+            </header>
+            <main class="container">
+                <div class="row">
 
-                                ${managerCard(manager)}
-                                ${engineerCard(engineer)}
-                                ${internCard(intern)}
+                    ${managerCard(manager)}
+                    ${engineerCard(engineer)}
+                    ${internCard(intern)}
+
+                </div>
 
 
-                                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-                                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-                            </body>
+                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+            </main>
+        </body>
     </html>`;
 }
 
